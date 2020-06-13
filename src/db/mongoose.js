61f178eaa89a5605d9aee785b0df10f48e4666e2
lucaspere <lucasrupestre@hotmail.com>
2020-06-13 13:44:06 -0300

    configurando o task

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,24 +2,5 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/tasks-manager-api', {
    useNewUrlParser: true,
-   useUnifiedTopology: true,
    useCreateIndex: true
-})
-
-
-const Task = mongoose.model('Task', {
-   description: {
-      type: String,
-      trim: true,
-      required: true
-   },
-   completed: {
-      type: Boolean,
-      default: false
-   }
-})
-
-const estudar = new Task({
-   description: "Revisar os conceitos de nodejs apis",
-   completed: false
 })
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 require('./db/mongoose');
 
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,7 +16,17 @@ app.post('/users', (req, res) => {
    user.save().then((result) => {
       res.status(201).send(result);
    }).catch((err) => {
-      res.send(err)
+      res.status(400).send(err)
+   })
+})
+
+app.post('/tasks', (req, res) => {
+   const task = new Task(req.body);
+
+   task.save().then((result) => {
+      res.status(201).send(result)
+   }).catch((err) => {
+      res.status(400).send(err)
    })
 })
 
