f35411894c99775784693ca010b36b8eb45702aa
lucaspere <lucasrupestre@hotmail.com>
2020-06-13 15:50:10 -0300

    configurando a rota /users/:id

--- src/index.js
+++ src/index.js
@@ -14,9 +14,24 @@ app.get('/users', (req, res) => {
    User.find({}).then((result) => {
       res.status(200).send(result)
    }).catch((err) => {
-      res.status(404).send(err)
+      res.status(500).send(err)
    })
 })
+
+app.get('/users/:id', (req, res) => {
+   const  _id = req.params.id;
+console.log(req.params)
+   User.findById(_id).then((result) => {
+      if(!result) {
+         return res.status(404).send("Usuário não encontrado");
+      }
+
+      res.status(200).send(result)
+   }).catch((err) => {
+      res.status(500).send(err)
+   })
+})
+
 app.post('/users', (req, res) => {
    const user = new User(req.body);
 
